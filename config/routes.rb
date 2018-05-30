6aa687e198d95a18bb0da8247673a70b98bd5278
EC2 Default User <ec2-user@ip-172-31-47-162.us-east-2.compute.internal>
2018-05-30 04:20:40 +0000

    atualizado crud e login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  get 'static_pages/home'
   resources :tags
   resources :fornecedores
   resources :marcas
   resources :produtos
-  root 'produtos#index'
+  root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
